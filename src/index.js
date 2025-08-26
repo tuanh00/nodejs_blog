9dd33c1b37fd994b474d100f7cd082a4531007dd
Tu Anh <june265zz@gmail.com>
2025-08-26 12:48:56 -0400

    chore: Update existing courses 1. installed method-override: ensure form can be sent using PUT 2. created me.js for new route managing personal courses 3. created stored-courses display all courses in mongodb 4. created edit.hbs to display and save changes for selected courses

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const methodOverride = require('method-override')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 const route = require('./routes'); //import index.js automatically
+
+
 const db = require('./config/db');
 
 // Connect to DB
@@ -15,11 +18,16 @@ db.connect();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
+app.use(express.json());
 app.use(morgan('combined')); // or 'dev' in development
 
 app.engine('hbs', engine({
-    extname: '.hbs'
+    extname: '.hbs',
+    helpers: {
+      //Sum order in displayed table
+      sum: (a, b) => a + b
+    }
 }));             // defaults are fine
 app.set('view engine', 'hbs');           // uses .handlebars extension
 app.set('views', path.join(__dirname, 'resources', 'views'));
