75798d47e3b3a3969e1be7a076896a0aa992fa7d
Tu Anh <june265zz@gmail.com>
2025-08-26 07:59:54 -0400

    chore: Show details of each course and slug

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -1,11 +1,14 @@
 const Course = require("../models/Course");
-const { multipleMongooseToObject } = require('../../util/mongoose')
+const { mongooseToObject } = require("../../util/mongoose");
 
 class CourseController {
-
   //[GET] /courses/:slug
-  show(req, res) {
-    res.send("show details");
+  show(req, res, next) {
+    Course.findOne({ slug: req.params.slug })
+      .then(course => {
+        res.render('courses/show', { course: mongooseToObject(course) });
+      })
+      .catch(next);
   }
 }
 module.exports = new CourseController();
